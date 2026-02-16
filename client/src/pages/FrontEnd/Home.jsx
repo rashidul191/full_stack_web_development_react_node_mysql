d14b47aedda17a69801259ec1814445e82d77736
rashidul191 <rashidul.191cse.gub@gmail.com>
2026-02-16 10:54:20 +0600

    add react bootstrap cnd link and update readme.md file

--- client/src/pages/FrontEnd/Home.jsx
+++ client/src/pages/FrontEnd/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h4>This is Home Page</h4>
+            <h4 className='text-center'>This is Home Page</h4>
+            <button className='btn btn-primary'>Click Me</button>
         </div>
     );
 };
